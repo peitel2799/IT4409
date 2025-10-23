432eb51e272f8d371a663baf30b94fe0ce53a0d3
peitel279 <109862103+peitel2799@users.noreply.github.com>
2025-10-23 23:46:37 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,13 +15,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-//make ready for deployment
-if(process.env.NODE.env === "production") {
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
+  
     app.get("*", (_, res) => {
-        res.sendFile(path.join(__dirname), "../frontend", "dist", "index.html");
-    })
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 
