ee9477dd1987dd40eb3db9a58fcbfa0afac3c774
skullick <Tien.NM225099@sis.hust.edu.vn>
2025-10-25 16:17:13 +0700

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,32 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 
 dotenv.config();
 
-const app = express();
+const app = express(); 
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve()
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+    connectDB()
+});
 
