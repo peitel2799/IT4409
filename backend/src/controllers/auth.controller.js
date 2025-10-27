da266454c814d566983be3d25fa66ecbb6be0fc8
skullick <Tien.NM225099@sis.hust.edu.vn>
2025-10-27 14:42:37 +0700

    Login and logout added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
-import User from "../models/User";
+import User from "../models/User.js";
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
@@ -33,8 +33,8 @@ export const signup = async (req, res) => {
         })
 
         if(newUser) {
-            generateToken(newUser._id, res);
-            await newUser.save();
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id, res);
 
             res.status(201).json({
                 _id: newUser._id,
@@ -51,4 +51,35 @@ export const signup = async (req, res) => {
         console.log("Error in signup controller:", error);
         res.status(500).json({message: "Internal server error"});
     }
-}
+};
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email})
+        if(!user) return res.status(400).json({
+            message: "Invalid credentials"
+        });
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect) return res.status(400).json({message: "Invalid credentials"});
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.error("Error in login controller:", error);
+        res.status(500).json({message: "Interna; server error"});
+    }
+};
+
+export const logout = (_, res) => {
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({message: "Logged out succesfully"});
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
@@ -12,7 +13,7 @@ dotenv.config();
 const app = express(); 
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT;
 
 app.use(express.json())
 
@@ -20,7 +21,7 @@ app.use(express.json())
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if(process.env.NODE_ENV === 'production'){
+if(ENV.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (_, res) => {
