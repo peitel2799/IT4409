edb407decfab7462beac2d7822ed0c8b4bf7b6e3
peitel279 <109862103+peitel2799@users.noreply.github.com>
2025-11-16 15:12:13 +0700

    change to build

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,10 +22,10 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
   });
 }
 
