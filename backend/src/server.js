cc4b1575ed586830beab5703f7a89b29310c34d1
peitel279 <109862103+peitel2799@users.noreply.github.com>
2025-11-16 15:06:39 +0700

    test deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,25 +11,25 @@ import { app, server } from "./lib/socket.js";
 
 const __dirname = path.resolve();
 
-const PORT = ENV.PORT || 3000;
+const PORT = ENV.PORT || 5173;
 
 app.use(express.json({ limit: "5mb" })); // req.body
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
-<<<<<<< HEAD
-app.use(express.json()); // Middleware to parse JSON request.body
-app.use(cookieParser())
-=======
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
->>>>>>> tien
 
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
+server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
   connectDB();
 });
